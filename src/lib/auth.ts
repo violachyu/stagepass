26e5bfcdbfee5c91a849ee369448534297a953fa
rc945 <rc945@cornell.edu>
2025-05-08 22:00:19 -0400

    google auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,13 @@ import { db } from "../../database/db"
 import * as schema from "../../database/schema"
 
 export const auth = betterAuth({
+    socialProviders: {
+        google: { 
+            prompt: "select_account",
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
     database: drizzleAdapter(db, {
         provider: "pg",
         usePlural: true,
